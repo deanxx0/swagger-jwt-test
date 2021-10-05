1157bbc29ce5cc0c803be8f6165b3fe936656c1e
deanxx <deanxx999@gmail.com>
2021-10-05 16:11:01 +0900

    create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })

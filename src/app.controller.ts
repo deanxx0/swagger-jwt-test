10c62db17be4f0bb5bd301de49101ae7268e3e81
deanxx <deanxx999@gmail.com>
2021-10-07 09:49:06 +0900

    add swagger infos on controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,22 @@
 import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { UserDto } from './user/user.dto';
-import { UserDocument } from './user/user.schema';
+import { User, UserDocument } from './user/user.schema';
 import { UserService } from './user/user.service';
 
 @Controller()
+@ApiTags('jwtlogin')
 export class AppController {
   constructor(private authService: AuthService, private userService: UserService) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
+  @ApiOperation({ summary: 'id,pw 인증', description: 'token 발행' })
+  @ApiCreatedResponse({ description: 'token 발행', type: User })
   async login(@Request() req) {
     console.log(`Post auth/login!`);
     return this.authService.login(req.user);
